e6945d1f11ffcb24917a7150e9eb095bc540fb04
TurtleSmile5110 <turtlesmile.contact@gmail.com>
2026-02-01 14:01:19 +0100

    Added select system for kebabs

--- main/script.js
+++ main/script.js
@@ -3,12 +3,18 @@ import { checkPath } from "../src/path_checker.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
     const userData = await getUserData("");
+    const superAdmin = document.querySelectorAll(".super-admin");
 
     document.getElementById("main-loader").classList.add("hide");
     document.body.classList.add("validated");
 
     if (userData.rank >= 1) {
         await initApp();
+        if (userData.rank >= 5) {
+            superAdmin.forEach(elem => {
+                elem.style.display = "block";
+            });
+        }
         document.getElementById("staff-panel").style.display = "block";
         document.getElementById("access-loading").style.display = "none";
         document.getElementById("access-denied").style.display = "none";
@@ -24,6 +30,22 @@ document.addEventListener("DOMContentLoaded", async () => {
 });
 
 async function initApp() {
+    window.see_kebabs = async function () {
+        window.location.href = ("../potimarrons/index.html");
+    }
+
+    window.add_kebab = async function () {
+        alert("Cette fonctionnalité n'est pas encore disponible.");
+    }
+
+    window.remove_kebab = async function () {
+        alert("Cette fonctionnalité n'est pas encore disponible.");
+    }
+
+    window.edit_kebab = async function () {
+        alert("Cette fonctionnalité n'est pas encore disponible.");
+    }
+
     window.redirect = async function () {
         window.location.href = ("../admin/index.html");
     }
